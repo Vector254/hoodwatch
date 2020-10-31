fc00dc5bda40ebb9359dc4e7241ca7bc033ffc75
Vector254 <ochrist7@gmail.com>
2020-10-31 08:35:13 +0300

    configure urls

--- hoodwatch/apps.py
+++ hoodwatch/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class HoodwatchConfig(AppConfig):
     name = 'hoodwatch'
+
+    def ready(self):
+        import hoodwatch.signals
--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hoodwatch.apps.HoodwatchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
