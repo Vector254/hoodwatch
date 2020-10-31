c3e68a5200a74e4b8232c56ff1724d66f67ba7d1
Vector254 <ochrist7@gmail.com>
2020-10-31 08:45:42 +0300

    implement user creation

--- hoodwatch/views.py
+++ hoodwatch/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created successfully!')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/registration_form.html',{'form':form})
