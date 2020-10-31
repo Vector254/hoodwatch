63d3dbcf3baaa2c2f119b7a9fbe0cea1263e52c0
Vector254 <ochrist7@gmail.com>
2020-10-31 09:35:25 +0300

    register models

--- hoodwatch/admin.py
+++ hoodwatch/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, NeighbourHood, Business
 
 # Register your models here.
+admin.site.Register(Profile)
+admin.site.Register(NeighbourHood)
+admin.site.Register(Business)
