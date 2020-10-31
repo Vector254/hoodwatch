5cf4f0213e1da17c40640158b0b1e53985cd36c0
Vector254 <ochrist7@gmail.com>
2020-10-31 08:42:32 +0300

    create account routes

--- hoodwatch/urls.py
+++ hoodwatch/urls.py
@@ -2,11 +2,15 @@ from django.conf.urls import url, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url('accounts/', include('django.contrib.auth.urls')),
+    url('register/', views.register, name='register'),
+    url('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
