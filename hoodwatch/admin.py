eb1c2c11e0a4062f4eede02baf56f13a4cff845c
Vector254 <ochrist7@gmail.com>
2020-10-31 09:39:25 +0300

    configure postgress db

--- hoodwatch/admin.py
+++ hoodwatch/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Profile, NeighbourHood, Business
 
 # Register your models here.
-admin.site.Register(Profile)
-admin.site.Register(NeighbourHood)
-admin.site.Register(Business)
+admin.site.register(Profile)
+admin.site.register(NeighbourHood)
+admin.site.register(Business)
--- project/settings.py
+++ project/settings.py
@@ -79,9 +79,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'hood',
+            'USER':'vector',
+            'PASSWORD':'2547Viki',
+           
+        }
 }
 
 
